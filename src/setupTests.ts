a2335b2e2630ebe7e307ad89482c5e414405a711
ManuGO <manuel.gomez@wolox.co>
2021-09-17 07:26:57 -0500

    Added first test

---
+++ src/setupTests.ts
@@ -0,0 +1,26 @@
+import 'regenerator-runtime/runtime';
+import '@testing-library/jest-dom';
+import 'mutationobserver-shim';
+
+jest.mock('react-i18next', () => ({
+  initReactI18next: {
+    type: '3rdParty',
+    init: () => {}
+  },
+  useTranslation: () => ({
+    t: (key: string, params: Record<string, string>) => (params ? `${key} ${JSON.stringify(params)}` : key),
+    i18n: {
+      changeLanguage: jest.fn(),
+      language: 'en'
+    }
+  })
+}));
+
+jest.mock('i18next', () => ({
+  addResources: jest.fn(),
+  use: () => ({ init: jest.fn() })
+}));
+
+Object.defineProperty(window, 'location', {
+  value: { assign: jest.fn() }
+});
